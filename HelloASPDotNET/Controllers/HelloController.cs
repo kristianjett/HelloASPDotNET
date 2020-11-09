8620c6f66f887617188f6de84088e0601f05da22
Kristian <jettkristian@gmail.com>
2020-11-08 22:25:02 -0800

    Add project files.

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HelloASPDotNET.Controllers
+{
+    //[Route("/hello")]
+    public class HelloController : Controller
+    {
+
+        // GET: /<controller>
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: /<controller>/welcome
+        [HttpPost("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            ViewBag.person = name;
+
+            return View();
+        }
+
+        //public static string CreateMessage(string name, string language)
+        //{
+        //    string greeting = $"Hello, {name}!";
+
+        //    if (language == "spanish")
+        //    {
+        //        greeting = $"Hola, {name}!";
+
+        //        return greeting;
+        //    }
+            
+        //    if (language == "danish")
+        //    {
+        //        greeting = $"Hej, {name}!";
+
+        //        return greeting;
+        //    }
+            
+        //    if (language == "french")
+        //    {
+        //        greeting = $"Bonjour, {name}!";
+
+        //        return greeting;
+        //    }
+            
+        //    if (language == "pirate")
+        //    {
+        //        greeting = $"Ahoy, {name}!";
+
+        //        return greeting;
+        //    }
+            
+
+        //    return greeting;
+        //}
+    }
+}
